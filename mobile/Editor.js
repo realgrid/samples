42573c753416a210ca40de986ee3ac8286b06fb3
tacrosst <tacrosst@realgrid.com>
2019-05-29 10:03:19 +0900

    mobileEditor 추가

---
+++ mobile/Editor.js
@@ -0,0 +1,176 @@
+var grdMain, dataProvider;
+
+$(document).ready(function() {
+    $("html, body").css({
+        width : "100%",
+        height : "100%"
+    });
+    $("#grdMain").css({
+        width : "100%",
+        height : "95%"
+    });
+    RealGridJS.setRootContext("../lib");
+    
+    dataProvider = new RealGridJS.LocalDataProvider();
+    grdMain = new RealGridJS.GridView("grdMain");
+    grdMain.setDataSource(dataProvider);
+
+    setFields(dataProvider);
+    setColumns(grdMain);
+    setOptions(grdMain, dataProvider);
+    
+    grdMain.setStyles(generalBlueSkin);
+    
+    cfnOrientationGrid(grdMain);
+    
+    loadData(dataProvider);
+    setMobileOptions();
+});
+
+function setFields(provider) {
+    var fields = [{
+        fieldName : "OrderID"
+    }, {
+        fieldName : "CustomerID"
+    }, {
+        fieldName : "EmployeeID"
+    }, {
+        fieldName : "OrderDate",
+        dataType : "datetime"
+    }, {
+        fieldName : "CompanyName"
+    }, {
+        fieldName: "Quantity",
+        dataType: "number"
+    }];
+
+    provider.setFields(fields);
+}
+
+function setColumns(grid) {
+    var columns = [{
+        name : "OrderID",
+        fieldName : "OrderID",
+        type : "data",
+        width : 90*2,
+        styles : {
+            textAlignment : "text"
+        },
+        header : {
+            text : "Text editor"
+        }
+    }, {
+        name : "CustomerID",
+        fieldName : "CustomerID",
+        width : 130*2,
+        lookupDisplay: true,
+        values: [
+            "VINET",
+            "HANAR",
+            "SUPRD",
+            "VICTE",
+            "THREE",
+            "SEVEN",
+            "TOMSP"
+        ],
+        labels: [
+            "<VINET>",
+            "<HANAR>",
+            "<SUPRD>",
+            "<VICTE>",
+            "<THREE>",
+            "<SEVEN>",
+            "<TOMSP>"
+        ],
+        editor: {
+            type: "dropDown",
+            dropDownCount: 4
+        },
+        styles: {
+            textAlignment: "center"
+        },
+        header: {
+            text: "DropDown Edit"
+        }
+    }, {
+        name : "OrderDate",
+        fieldName : "OrderDate",
+        width : 130*2,
+        editor: {
+            type: "date",
+            datetimeFormat: "yyyy.MM.dd"
+        },
+        styles: {
+            textAlignment: "center",
+            datetimeFormat: "yyyy.MM.dd"
+        },
+        header : {
+            text : "Date Edit"
+        }
+    }, {
+        name : "CompanyName",
+        fieldName : "CompanyName",
+        width : 100*2,
+        editor: {
+            type: "multiline",
+            textCase: "upper"
+        },
+        styles: {
+            textAlignment: "near",
+            textWrap: "normal"
+        },
+        header: {
+            text: "Multiline Edit"
+        }
+    }, {
+        nam: "Quantity",
+        fieldName: "Quantity",
+        width: 100*2,
+        editor: {
+            type: "number",
+            textAlignment: "far",
+            editFormat: "#,##0.##",
+            multipleChar: "+"
+        },
+        styles: {
+            textAlignment: "far",
+            numberFormat: "#,##0.##"
+        },
+        header: {
+            text: "Number Edit"
+        }
+    }];
+
+    grid.setColumns(columns);
+}
+
+function setOptions(grid, provider) {
+    grid.setOptions({
+        display: {
+            fitStyle : "even"
+        }
+    });
+    provider.setOptions({
+        datetimeFormat : "yyyy-MM-dd a hh:mm:ss",
+        amText : "오전",
+        pmText : "오후"
+    });
+}
+
+function loadData(provider) {
+    provider.fillJsonData(mobileData);
+}
+
+function fnRdoChange(e){
+    var style = this.value;
+    grdMain.orderBy([]);
+    grdMain.setSortingOptions({style : style});
+}
+
+function setMobileOptions() {   
+    grdMain.setEditOptions({editable: true})
+    grdMain.setMobileOptions({
+        longTapDuration: 300,
+        doubleTapInterval: 300
+    })
+}
