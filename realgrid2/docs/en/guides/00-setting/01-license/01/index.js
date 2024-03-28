8c3d6b3b2f05673652745ed1723993f6c7da7ea7
fanelia <fanelia@realgrid.com>
2024-03-28 15:48:27 +0900

    영어 데이터 경로 변경

--- realgrid2/docs/en/guides/00-setting/01-license/01/index.js
+++ realgrid2/docs/en/guides/00-setting/01-license/01/index.js
@@ -239,7 +239,7 @@ var httpRequest;
 function setProvider(filename) {
   httpRequest = new XMLHttpRequest();
   httpRequest.onreadystatechange = loadData;
-  httpRequest.open("GET", "/public/data/" + filename);
+  httpRequest.open("GET", "/public/data/en/" + filename);
   httpRequest.send();
 }
 
